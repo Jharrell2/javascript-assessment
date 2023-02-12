a9469755836c4a9650b62ef11ec809f870982eca
Jharrell2 <123315223+Jharrell2@users.noreply.github.com>
2023-02-12 15:46:45 -0500

    Add files via upload

---
+++ javascript-assessment/script.js
@@ -0,0 +1,10 @@
+let firstName = "Jevon";
+let lastName = " Harrell";
+statement = "My name is ";
+statement = statement + firstName + lastName;
+alert(statement);
+var age = 20
+statements = "I am "
+statements = statements + age + " years old";
+alert(statements)
+console.log("I am having fun in this class")
